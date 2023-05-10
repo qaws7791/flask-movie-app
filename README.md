eaf02aa8a53090c43722b9759c368110d0469c5b
qaws7791 <qaws7791@gmail.com>
2023-05-10 23:45:37 +0900

    mongodb와 연결 mongodb 와 연결하여 코멘트 저장, 불러오기 기능 추가

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+##Start Code
+python -m venv venv
+pip install flask pymongo
+
 ## hosting dashboard
 
 https://www.pythonanywhere.com/user/qaws7791/
@@ -5,3 +9,19 @@ https://www.pythonanywhere.com/user/qaws7791/
 ## hosting site
 
 http://qaws7791.pythonanywhere.com/
+
+## API
+
+|                |                                                           |
+| -------------- | --------------------------------------------------------- |
+| 영화 검색      | https://developers.themoviedb.org/3/search/search-movies  |
+| 영화 장르 목록 | https://developers.themoviedb.org/3/genres/get-movie-list |
+|                |                                                           |
+|                |                                                           |
+
+## PythonAnywhere
+
+- Venv: https://help.pythonanywhere.com/pages/Virtualenvs/
+- Flask: https://help.pythonanywhere.com/pages/Flask/
+- MongoDB:https://help.pythonanywhere.com/pages/MongoDB/
+- https://help.pythonanywhere.com/pages/environment-variables-for-web-apps
--- app.py
+++ app.py
@@ -1,23 +1,72 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, jsonify, json,url_for
+from pymongo import MongoClient
+
+# flask
 app = Flask(__name__)
 
+# mongodb
+load_dotenv()
+MONGODB_URI = os.getenv("MONGODB_URI")
+print(MONGODB_URI)
+client = MongoClient(MONGODB_URI)
+print(client)
+db = client['flask-movie-app']
+
+
+# movie genre list
+with open('./genre.json','rt',encoding='UTF8') as file:
+    jsonData = json.load(file)['genres']
+
+genreList = {}
+for genre in jsonData:
+    genreList[genre['id']] = genre['name']
+print(genreList)
+
+##### flask app #####
+
+## HOME
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+## create comment
+@app.route("/api/comment", methods=["POST"])
+def create_comment():
+    name = request.form['name']
+    comment = request.form['comment']
+    star = request.form['star']
+    movie_id = request.form['movie_id']
+    doc = {
+        'name': name,
+        'comment': comment,
+        'star': star,
+        'movie_id' : movie_id
+    }
+    result =  db['comment'].insert_one(doc)
+    print(result)
+    return jsonify({'msg': '저장 완료!'})
+
+## get comment list
+@app.route("/api/comment/<int:id>", methods=["GET"])
+def get_comments(id):
+    all_comments = list(db.comment.find({'movie_id':str(id)}, {'_id': False}))
+
+    return jsonify({'result': all_comments})
     
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+
 
 @app.route('/movie/<int:id>')
 def movie(id):
     return render_template('comment.html',id=id)
+    
+@app.route('/genre/<int:id>')
+def genre(id):
+    if id in genreList:
+        return render_template('genre.html',id=id)
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5050, debug=True)
